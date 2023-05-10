38f8a16a0d54eebfe6f70fcd012730d4d187ade6
BruvLio <lionel.mbayu@gmail.com>
2023-05-10 12:59:24 -0500

    wip

--- data/migrations/20230510170036_first-migration.js
+++ data/migrations/20230510170036_first-migration.js
@@ -3,7 +3,48 @@
  * @returns { Promise<void> }
  */
 exports.up = async function (knex) {
-  await knex.schema.createTable().createTable().createTable().createTable();
+  await knex.schema
+    .createTable("zoos", (table) => {
+      table.increments("zoo_id");
+      table.string("zoo_name", 128).notNullable();
+      table.string("address", 128).unique();
+    })
+    .createTable("species", (table) => {
+      table.increments("species_id");
+      table.string("species_name", 246).notNullable();
+      //   table.string("address").unique()
+    })
+    .createTable("animals", (table) => {
+      table.increments("animal_id");
+      table.string("animal_name", 128).notNullable();
+      table
+        .integer("species_id")
+        .unsigned()
+        .notNullable()
+        .references("species_id")
+        .inTable("species")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    })
+    .createTable("zoo_animals", (table) => {
+      table.increments("zoo_animal_id");
+      table
+        .integer("zoo_id")
+        .unsigned()
+        .notNullable()
+        .references("zoo_id")
+        .inTable("zoos")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      table
+        .integer("animal_id")
+        .unsigned()
+        .notNullable()
+        .references("animal_id")
+        .inTable("animals")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+    });
 };
 
 /**
@@ -12,8 +53,8 @@ exports.up = async function (knex) {
  */
 exports.down = async function (knex) {
   await knex.schema
-    .dropTableIfExists()
-    .dropTableIfExists()
-    .dropTableIfExists()
-    .dropTableIfExists();
+    .dropTableIfExists("zoo_animals")
+    .dropTableIfExists("animals")
+    .dropTableIfExists("species")
+    .dropTableIfExists("zoos");
 };
